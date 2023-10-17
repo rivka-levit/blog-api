b7217d88e6ad831923a4b03bf4a79c0ca3d985d3
rivka-yuliia <juliacreadora@gmail.com>
2023-10-17 06:18:06 +0300

    post admin registration

--- app/post/admin.py
+++ app/post/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from post.models import Category, Author
+from post.models import Category, Author, Post
 
 
 @admin.register(Category)
@@ -15,3 +15,10 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     list_display_links = ['name', 'slug']
     prepopulated_fields = {'slug': ['name']}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'author', 'created_at', 'updated_at']
+    prepopulated_fields = {'slug': ['title']}
+    readonly_fields = ['created_at', 'updated_at']
