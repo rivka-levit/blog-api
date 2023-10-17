1cd1d09c4507aec4c3694de3013a236fe3510f4a
rivka-yuliia <juliacreadora@gmail.com>
2023-10-17 17:08:02 +0300

    section admin inline

--- app/post/admin.py
+++ app/post/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from post.models import Category, Author, Post
+from post.models import Category, Author, Post, Section
 
 
 @admin.register(Category)
@@ -17,8 +17,15 @@ class AuthorAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
 
 
+class SectionInline(admin.TabularInline):
+    model = Section
+    fields = ['ordering', 'sub_title', 'content', 'user', 'post']
+    extra = 1
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'author', 'created_at', 'updated_at']
     prepopulated_fields = {'slug': ['title']}
     readonly_fields = ['created_at', 'updated_at']
+    inlines = [SectionInline]
